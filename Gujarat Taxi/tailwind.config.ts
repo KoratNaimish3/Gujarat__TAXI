38f2dd11ed811bc84dc8efee0ad233047926cb17
KoratNaimish3 <koratnaimish6@gmail.com>
2025-12-08 20:23:46 +0530

    fix light/dark mode

--- Gujarat Taxi/tailwind.config.ts
+++ Gujarat Taxi/tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: false, // Disable dark mode - always use light mode
+  darkMode: "class", // Disable dark mode - always use light mode
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
